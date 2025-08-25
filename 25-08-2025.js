ad6a800201f2fffb61321b7f11ae95269ba786ab
ShadowFox <gastonkowalsuk@gmail.com>
2025-08-25 20:04:11 +0000

    25 de agosto

---
+++ 25-08-2025.js
@@ -0,0 +1,7 @@
+let variable =1+1
+console.log(variable)
+//crea una variable que contenga tu nombre como valor
+let nombre = "thiago"
+// crea una variable que sume tu nombre y tu apellido
+let nombreCompleto = "thiago " + "kowalczuk"
+console.log(nombreCompleto)
