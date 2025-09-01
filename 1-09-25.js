2833296ab1ff44f675dac9c9685107f01935b63d
ShadowFox <gastonkowalsuk@gmail.com>
2025-09-01 20:17:51 +0000

    01-09-25

---
+++ 1-09-25.js
@@ -0,0 +1,6 @@
+function sumar(atributo1,atributo2){
+    let variableDevuelta = atributo1 + atributo2
+    return variableDevuelta
+}
+let Llamar_a_la_funcion= nombreDeLaFuncion(1,2)
+console.log(Llamar_a_la_funcion)
